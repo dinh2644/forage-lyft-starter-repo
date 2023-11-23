c1f8ab7a883621428a599140af623e709ed0e1a4
dinh2644 <dinh2644.mail@gmail.com>
2023-11-23 17:54:19 -0500

    refactored codebase

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,11 @@
+
+class NubbinBattery:
+    def __init__(self,last_service_date,current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        if self.current_date - self.last_service_date >= 4:
+            return True
+        else:
+            return False
