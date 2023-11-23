2fd5f7192fe12c825827ca5645609c926f86af3f
dinh2644 <dinh2644.mail@gmail.com>
2023-11-23 17:57:23 -0500

    refactored codebase

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,5 +1,6 @@
+from battery.battery import Battery
 
-class NubbinBattery:
+class NubbinBattery(Battery):
     def __init__(self,last_service_date,current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
--- battery/spindler_batter.py
+++ battery/spindler_batter.py
@@ -1,5 +1,6 @@
+from battery.battery import Battery
 
-class SpindlerBattery:
+class SpindlerBattery(Battery):
     def __init__(self,last_service_date,current_date):
         self.last_service_date = last_service_date
         self.current_date = current_date
